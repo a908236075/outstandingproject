c3982409ed6755c5b99e1010e299cc4ef85d8cfb
a908236075 <908236075@qq.com>
2022-07-26 23:12:00 +0800

    httpClient send post and upload file

--- src/test/java/com/liuliu/outstanding/httpClient/RestTemplateAndHttpClientTest.java
+++ src/test/java/com/liuliu/outstanding/httpClient/RestTemplateAndHttpClientTest.java
@@ -1,16 +1,28 @@
 package com.liuliu.outstanding.httpClient;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
+import org.apache.http.auth.AuthenticationException;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -36,6 +48,31 @@ class RestTemplateAndHttpClientTest {
         RequestConfig config = RequestConfig.custom().setConnectTimeout(timeout * 1000).setConnectionRequestTimeout(timeout * 1000).setSocketTimeout(timeout * 1000).build();
         CloseableHttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
         CloseableHttpResponse response = client.execute(new HttpGet("https://www.baidu.com/"));
+        int code = response.getStatusLine().getStatusCode();
+        Assertions.assertEquals(code, HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void sendPost() throws IOException, AuthenticationException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("https://www.baidu.com/");
+        httpPost.setEntity(new StringEntity("test post"));
+        UsernamePasswordCredentials creds = new UsernamePasswordCredentials("tom", "12345");
+        httpPost.addHeader(new BasicScheme().authenticate(creds, httpPost, null));
+        CloseableHttpResponse response = client.execute(httpPost);
+        int code = response.getStatusLine().getStatusCode();
+        Assertions.assertEquals(code, HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void whenUploadFileUsingHttpClient() throws IOException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("https://www.baidu.com/");
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+        builder.addBinaryBody("file", new File("test.txt"), ContentType.APPLICATION_OCTET_STREAM, "testFile.txt");
+        HttpEntity httpEntity = builder.build();
+        httpPost.setEntity(httpEntity);
+        CloseableHttpResponse response = client.execute(httpPost);
         int code = response.getStatusLine().getStatusCode();
         Assertions.assertEquals(code, HttpStatus.SC_OK);
     }
