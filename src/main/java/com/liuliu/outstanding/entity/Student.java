cdd670626a26b769753d2006f88aa50ae4d7ea73
a908236075 <908236075@qq.com>
2022-09-07 23:02:48 +0800

    RestTemplate content-type 设置以及上传文件

--- src/main/java/com/liuliu/outstanding/entity/Student.java
+++ src/main/java/com/liuliu/outstanding/entity/Student.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -20,6 +22,7 @@ import lombok.experimental.Accessors;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @TableName("student")
+@AllArgsConstructor
 public class Student implements Serializable {
 
     private static final long serialVersionUID = 1L;
--- src/main/java/com/liuliu/outstanding/httpClient/RestTemplateAndHttpClient.java
+++ src/main/java/com/liuliu/outstanding/httpClient/RestTemplateAndHttpClient.java
@@ -1,11 +1,41 @@
 package com.liuliu.outstanding.httpClient;
 
-public class RestTemplateAndHttpClient {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.File;
+
+public class RestTemplateAndHttpClient {
+    @Autowired
+    private RestTemplate restTemplate;
 
     public void sentHttp() {
+        MultiValueMap<String, Object> body
+                = new LinkedMultiValueMap<>();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        File file = new File("");
+        body.add("files", new FileSystemResource(file));
+        body.add("files", getTestFile());
+        body.add("files", getTestFile());
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        String serverUrl = "http://localhost:8082/spring-rest/fileserver/multiplefileupload/";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate
+                .postForEntity(serverUrl, requestEntity, String.class);
+    }
 
-
+    private Object getTestFile() {
+        return "yes";
     }
 
 
